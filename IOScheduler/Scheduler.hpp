5ccfa65898448c17f948b9c035a6573e8d461cce
bz866 <bz866@nyu.edu>
2019-09-03 20:22:39 -0400

    reformat and comments

--- IOScheduler/Scheduler.hpp
+++ IOScheduler/Scheduler.hpp
@@ -1,12 +1,6 @@
 
+//
 // Scheduler.hpp
-//
-//
-//
-//
-//
-//
-// Lab 4 exercise Operating System 2019 Summer
 //
 // Author: Binqian Zeng
 //
@@ -18,13 +12,14 @@
 The schedulers to be implemented are FIFO (i), SSTF (j), LOOK (s), CLOOK (c), and FLOOK (f) 
 (the letters in bracket define which parameter must be given in the –s program flag).
 
-
-Ref: 
-
-To Run:
-
-
 Note:
+
+IO Scheduler algorithms
+- FIFO(i)
+- SSTF(j)
+- LOOK(s)
+- CLOOK(c)
+- FLOOK(f)
 
 */
 //
@@ -107,4 +102,4 @@ public:
 };
 
 
-#endif
+#endif /* The IO Schduler defined */
